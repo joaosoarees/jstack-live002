1c68b0c50b8f1a7480527c8b23fa27fe3318cb50
Joao Vitor Alves Soares <joao.soares98@icloud.com>
2024-07-21 14:38:03 -0300

    feat: create store

---
+++ src/store/createStore.ts
@@ -0,0 +1,36 @@
+type SetterFn<T> = (prevState: T) => Partial<T>;
+
+export function createStore<TState>(initialState: TState) {
+  let state = initialState;
+  const listeners = new Set<() => void>();
+
+  function notifyListeners() {
+    listeners.forEach((listener) => listener());
+  }
+
+  function setState(partialState: Partial<TState> | SetterFn<TState>) {
+    const newValue =
+      typeof partialState === 'function' ? partialState(state) : partialState;
+
+    state = {
+      ...state,
+      ...newValue,
+    };
+
+    notifyListeners();
+  }
+
+  function subscribe(listener: () => void) {
+    listeners.add(listener);
+
+    return () => {
+      listeners.delete(listener);
+    };
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return { setState, getState, subscribe };
+}
