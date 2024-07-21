7587c1b96319b112bfbb9c1e2a77e4f99c0cf9b1
Joao Vitor Alves Soares <joao.soares98@icloud.com>
2024-07-21 20:24:40 -0300

    feat: use sefl state manager

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,8 +1,13 @@
-type SetterFn<T> = (prevState: T) => Partial<T>;
+import { useSyncExternalStore } from 'react';
 
-export function createStore<TState>(initialState: TState) {
-  let state = initialState;
-  const listeners = new Set<() => void>();
+type SetterFn<T> = (prevState: T) => Partial<T>;
+type SetStateFn<T> = (partialState: Partial<T> | SetterFn<T>) => void;
+
+export function createStore<TState extends Record<string, any>>(
+  createState: (setState: SetStateFn<TState>, getState: () => TState) => TState,
+) {
+  let state: TState;
+  let listeners: Set<() => void>;
 
   function notifyListeners() {
     listeners.forEach((listener) => listener());
@@ -32,5 +37,16 @@ export function createStore<TState>(initialState: TState) {
     return state;
   }
 
-  return { setState, getState, subscribe };
+  function useStore<TValue>(
+    selector: (currentState: TState) => TValue,
+  ): TValue {
+    const value = useSyncExternalStore(subscribe, () => selector(state));
+
+    return value;
+  }
+
+  state = createState(setState, getState);
+  listeners = new Set();
+
+  return useStore;
 }
